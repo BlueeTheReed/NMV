0c27d74c720d0af5be93e9a343f408041d2de414
Jonathan Sid-Otmane <Watabanga@gmail.com>
2015-10-07 12:02:35 +0200

    [MOD] goto
    L'un des objectifs de l'exercice 8 était en réalité les goto

--- comment.c
+++ comment.c
@@ -5,8 +5,8 @@
 #include"comment.h"
 
 struct comment *new_comment(int title_size, char *title,
-			    int author_size, char *author, int text_size,
-			    char *text)
+		int author_size, char *author, int text_size,
+		char *text)
 {
 	struct comment *c = (struct comment *)malloc(sizeof(struct comment));
 
@@ -17,34 +17,42 @@ struct comment *new_comment(int title_size, char *title,
 	 text_size = strlen(text) + 1;
 	 */
 
+	/*
 	c->title = strdup(title);
 	c->author = strdup(author);
 	c->text = strdup(text);
+	*/
 
-	/*
-	   c->title_size = title_size;
-	   if(! (c->title = malloc(title_size))){
-	   perror("malloc title");
-	   return NULL;
-	   }
-	   memcpy(c->title, title, title_size);
+	c->title_size = title_size;
+	if(! (c->title = malloc(title_size))){
+		perror("malloc title");
+		return NULL;
+	}
+	memcpy(c->title, title, title_size);
 
-	   c->author_size = author_size;
-	   if(! (c->author = malloc(author_size))){
-	   perror("malloc author");
-	   return NULL;
-	   }
-	   memcpy(c->author, author, author_size);
+	c->author_size = author_size;
+	if(! (c->author = malloc(author_size))){
+		perror("malloc author");
+		goto err_author;
+	}
+	memcpy(c->author, author, author_size);
 
-	   c->text_size = text_size;
-	   if(! (c->text = malloc(text_size))){
-	   perror("malloc text");
-	   return NULL;
-	   }
-	   memcpy(c->text, text, text_size);
-	 */
+	c->text_size = text_size;
+	if(! (c->text = malloc(text_size))){
+		perror("malloc text");
+		goto err_text;
+	}
+	memcpy(c->text, text, text_size);
 
 	return c;
+
+err_text:
+	free(c->author);
+	c->author = NULL;
+err_author:
+	free(c->title);
+	c->title = NULL;
+	return NULL;
 }
 
 void display_comment(struct comment *c)
