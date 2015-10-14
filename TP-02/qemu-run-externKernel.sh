62516f2148c78be49a75d570b912848e054e9e43
Jonathan Sid-Otmane <Watabanga@gmail.com>
2015-10-14 15:51:35 +0200

    [ADD] Fin de l'exercice 4, sur un échec!
    Voir avec les profs quant à la validité de l'énoncé!

---
+++ TP-02/qemu-run-externKernel.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# Fixer les variables avec les chemins de vos fichiers
+HDA=~/NMV/nmv/nmv-tp.img 
+HDB=~/NMV/nmv/myHome.img 
+KERNEL=/tmp/linux-4.2.3/arch/x86/boot/bzImage 
+
+# Si besoin ajouter une option pour le kernel
+#CMDLINE='root=/dev/sda1 rw vga=792 console=ttyS0 kgdboc=ttyS1  init=/bin/bash'
+CMDLINE='root=/dev/sda1 rw vga=792 console=ttyS0 kgdboc=ttyS1'
+
+#Exemple d'utilisation du initrd
+#exec qemu-system-x86_64 -hda "${HDA}" -hdb "${HDB}" -serial stdio -serial pty -net nic -net user  -boot c -m 1G -kernel "${KERNEL}" -initrd /tmp/m.cpio.gz  -append "${CMDLINE}"
+exec qemu-system-x86_64 -hda "${HDA}" -hdb "${HDB}" -serial stdio -serial pty -net nic -net user  -boot c -m 1G -kernel "${KERNEL}" -initrd /tmp/m.cpio.gz  -append "${CMDLINE}"
+
